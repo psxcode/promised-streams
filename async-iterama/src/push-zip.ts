239c9340dc9ec1ddd24110080c9ad871e2746474
psxcode <psxcode@gmail.com>
2019-02-19 22:25:15 +0100

    wip

--- async-iterama/src/push-zip.ts
+++ async-iterama/src/push-zip.ts
@@ -1,28 +1,19 @@
-import { PushConsumer, PullProducer } from './types'
-import { doneAsyncIteratorResult, asyncIteratorResult, errorAsyncIteratorResult } from './helpers'
+import { PushConsumer, PushProducer } from './types'
 
-function pushZip<T0> (p0: PullProducer<T0>): (consumer: PushConsumer<[T0]>) => Promise<void>
-function pushZip<T0, T1> (p0: PullProducer<T0>, p1: PullProducer<T1>): (consumer: PushConsumer<[T0, T1]>) => Promise<void>
-function pushZip<T0, T1, T2> (p0: PullProducer<T0>, p1: PullProducer<T1>, p2: PullProducer<T2>): (consumer: PushConsumer<[T0, T1, T2]>) => Promise<void>
-function pushZip<T0, T1, T2, T3> (p0: PullProducer<T0>, p1: PullProducer<T1>, p2: PullProducer<T2>, p3: PullProducer<T3>): (consumer: PushConsumer<[T0, T1, T2, T3]>) => Promise<void>
+function pushZip<T0> (p0: PushProducer<T0>): (consumer: PushConsumer<[T0]>) => Promise<void>
+function pushZip<T0, T1> (p0: PushProducer<T0>, p1: PushProducer<T1>): (consumer: PushConsumer<[T0, T1]>) => Promise<void>
+function pushZip<T0, T1, T2> (p0: PushProducer<T0>, p1: PushProducer<T1>, p2: PushProducer<T2>): (consumer: PushConsumer<[T0, T1, T2]>) => Promise<void>
+function pushZip<T0, T1, T2, T3> (p0: PushProducer<T0>, p1: PushProducer<T1>, p2: PushProducer<T2>, p3: PushProducer<T3>): (consumer: PushConsumer<[T0, T1, T2, T3]>) => Promise<void>
 
-function pushZip (...producers: PullProducer<any>[]) {
-  return async (consumer: PushConsumer<any[]>): Promise<void> => {
-    while (true) {
-      let res: IteratorResult<any>[]
-      try {
-        res = await Promise.all(producers.map((p) => p()))
-      } catch (e) {
-        await consumer(errorAsyncIteratorResult(e))
-        continue
+function pushZip (...producers: PushProducer<any>[]): PushProducer<any> {
+  const values = producers.map(() => [])
+
+  return async (consumer) => {
+    return Promise.all(producers.map((p, i) => p(
+      async (result) => {
+
       }
-
-      if (res.some((r) => r.done)) {
-        return consumer(doneAsyncIteratorResult())
-      }
-
-      await consumer(asyncIteratorResult(res.map((r) => r.value)))
-    }
+    ))) as Promise<any>
   }
 }
 
---
+++ async-iterama/test/push-start-with.spec.ts
@@ -0,0 +1,120 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import debug from 'debug'
+import fn from 'test-fn'
+import { pushConsumer, pushProducer } from 'async-iterama-test/src'
+import { pushStartWith } from '../src'
+import makeNumbers from './make-numbers'
+
+const producerLog = debug('ai:producer')
+const consumerLog = debug('ai:consumer')
+const sinkLog = debug('ai:sink')
+
+describe('[ pushStartWith ]', () => {
+  it('should work', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog })(spy)
+    const t = pushStartWith(-1)
+    const r = pushProducer({ log: producerLog })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: -1, done: false }],
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 2, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
+  })
+
+  it('should work with multiple values', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog })(spy)
+    const t = pushStartWith(-3, -2, -1)
+    const r = pushProducer({ log: producerLog })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: -3, done: false }],
+      [{ value: -2, done: false }],
+      [{ value: -1, done: false }],
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 2, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
+  })
+
+  it('should work with no values', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog })(spy)
+    const t = pushStartWith()
+    const r = pushProducer({ log: producerLog })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 2, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
+  })
+
+  it('should deliver consumer cancel', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog, cancelAtStep: 1 })(spy)
+    const t = pushStartWith(3, 4, 5)
+    const r = pushProducer({ log: producerLog })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: 3, done: false }],
+      [{ value: 4, done: false }],
+    ])
+  })
+
+  it('should deliver producer error to consumer', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog })(spy)
+    const t = pushStartWith(32)
+    const r = pushProducer({ log: producerLog, errorAtStep: 2 })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: 32, done: false }],
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+    ])
+  })
+
+  it('should be able to continue after producer error', async () => {
+    const data = makeNumbers(4)
+    const spy = fn(sinkLog)
+    const w = pushConsumer({ log: consumerLog, continueOnError: true })(spy)
+    const t = pushStartWith(32)
+    const r = pushProducer({ log: producerLog, errorAtStep: 2 })(data)
+
+    await r(t(w))
+
+    expect(spy.calls).deep.eq([
+      [{ value: 32, done: false }],
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
+  })
+})
