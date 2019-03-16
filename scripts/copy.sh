e2d28036fcb44e6da6de7098172bfadd55a4d925
psxcode <psxcode@gmail.com>
2019-03-16 17:34:38 +0100

    fix copy script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-usage(){
-	echo "Usage: $0 path"
-	exit 1
-}
+cp LICENSE promise-streams/build/
+cp readme.md promise-streams/build/
+cp promise-streams/package.json promise-streams/build/
 
-is_package_dir(){
-	local f="$1/package.json"
-  local buildDir="$1/build"
-	[[ -f "$f" ]] && [[ -d "$buildDir" ]] && return 0 || return 1
-}
+cp LICENSE promise-streams-test/build/
+cp promise-streams-test/package.json promise-streams-test/build/
 
-[[ $# -eq 0 ]] && usage
-
-if ( is_package_dir "$1" )
-then
-  cp LICENSE "$1/build"
-  cp readme.md "$1/build"
-  cp "$1/package.json" "$1/build"
-else
- echo "No build directory"
- exit 1
-fi
