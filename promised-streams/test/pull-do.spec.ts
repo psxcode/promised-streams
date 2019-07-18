0df5997523c098c324f98910342c34b540f7e0ba
alex.feinstein <alex.feinstein@klarna.com>
2019-07-18 10:01:02 +0200

    add more tests

--- promised-streams/test/pull-do.spec.ts
+++ promised-streams/test/pull-do.spec.ts
@@ -4,90 +4,105 @@ import debug from 'debug'
 import fn from 'test-fn'
 import { waitTimePromise as wait } from '@psxcode/wait'
 import { pullConsumer, pullProducer } from 'promised-streams-test/src'
-import { pullMap } from '../src'
+import { pullDo } from '../src'
 import makeNumbers from './make-numbers'
 
 const producerLog = debug('ai:producer')
 const consumerLog = debug('ai:consumer')
-const mapLog = debug('ai:map')
+const doLog = debug('ai:do')
 const sinkLog = debug('ai:sink')
-const mult2 = (value: number) => {
-  mapLog('mapping value')
-
-  return value * 2
+const doFunc = (value: number) => {
+  doLog(`got the value ${value}`)
 }
 
-const amult2 = async (value: number) => {
-  mapLog('mapping value begin')
+const asyncDoFunc = async (value: number) => {
+  doLog(`getting value begin ${value}`)
   await wait(50)
-  mapLog('mapping value done')
-
-  return value * 2
+  doLog('getting value done')
 }
 
-const emult2 = () => {
-  throw new Error('error in mapper')
+const errorDoFunc = () => {
+  throw new Error('error')
 }
 
 
-describe('[ pullMap ]', () => {
+describe('[ pullDo ]', () => {
   it('should work', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pullConsumer({ log: consumerLog })(spy)
-    const t = pullMap(mult2)
+    const t = pullDo(doSpy)
     const r = pullProducer({ log: producerLog })(data)
 
     await w(t(r))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
       [{ value: 2, done: false }],
-      [{ value: 4, done: false }],
-      [{ value: 6, done: false }],
+      [{ value: 3, done: false }],
       [{ value: undefined, done: true }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
+      [2],
+      [3],
     ])
   })
 
-  it('should work with async map', async () => {
+  it('should work with async dofunc', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(asyncDoFunc)
     const w = pullConsumer({ log: consumerLog })(spy)
-    const t = pullMap(amult2)
+    const t = pullDo(doSpy)
     const r = pullProducer({ log: producerLog })(data)
 
     await w(t(r))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
       [{ value: 2, done: false }],
-      [{ value: 4, done: false }],
-      [{ value: 6, done: false }],
+      [{ value: 3, done: false }],
       [{ value: undefined, done: true }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
+      [2],
+      [3],
     ])
   })
 
-  it('should deliver mapper error to consumer', async () => {
+  it('should not deliver dofunc error to consumer', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
     const w = pullConsumer({ log: consumerLog })(spy)
-    const t = pullMap(emult2)
+    const t = pullDo(errorDoFunc)
     const r = pullProducer({ log: producerLog })(data)
 
-    try {
-      await w(t(r))
+    await w(t(r))
 
-      expect.fail('should not get here')
-    } catch {
-      expect(spy.calls).deep.eq([])
-    }
+    expect(spy.calls).deep.eq([
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 2, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
   })
 
   it('should deliver producer error to consumer', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pullConsumer({ log: consumerLog })(spy)
-    const t = pullMap(mult2)
+    const t = pullDo(doSpy)
     const r = pullProducer({ log: producerLog, errorAtStep: 2 })(data)
 
     try {
@@ -97,7 +112,12 @@ describe('[ pullMap ]', () => {
     } catch {
       expect(spy.calls).deep.eq([
         [{ value: 0, done: false }],
-        [{ value: 2, done: false }],
+        [{ value: 1, done: false }],
+      ])
+
+      expect(doSpy.calls).deep.eq([
+        [0],
+        [1],
       ])
     }
   })
@@ -105,25 +125,33 @@ describe('[ pullMap ]', () => {
   it('should deliver producer error to consumer and continue', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pullConsumer({ log: consumerLog, continueOnError: true })(spy)
-    const t = pullMap(mult2)
+    const t = pullDo(doSpy)
     const r = pullProducer({ log: producerLog, errorAtStep: 2 })(data)
 
     await w(t(r))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
-      [{ value: 2, done: false }],
-      [{ value: 6, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 3, done: false }],
       [{ value: undefined, done: true }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
+      [3],
     ])
   })
 
   it('should handle producer crash', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pullConsumer({ log: consumerLog })(spy)
-    const t = pullMap(mult2)
+    const t = pullDo(doSpy)
     const r = pullProducer({ log: producerLog, crashAtStep: 2 })(data)
 
     try {
@@ -133,7 +161,12 @@ describe('[ pullMap ]', () => {
     } catch {
       expect(spy.calls).deep.eq([
         [{ value: 0, done: false }],
-        [{ value: 2, done: false }],
+        [{ value: 1, done: false }],
+      ])
+
+      expect(doSpy.calls).deep.eq([
+        [0],
+        [1],
       ])
     }
   })
--- promised-streams/test/push-do.spec.ts
+++ promised-streams/test/push-do.spec.ts
@@ -4,122 +4,158 @@ import debug from 'debug'
 import fn from 'test-fn'
 import { waitTimePromise as wait } from '@psxcode/wait'
 import { pushConsumer, pushProducer } from 'promised-streams-test/src'
-import { pushMap } from '../src'
+import { pushDo } from '../src'
 import makeNumbers from './make-numbers'
 
 const producerLog = debug('ai:producer')
 const consumerLog = debug('ai:consumer')
 const mapLog = debug('ai:map')
 const sinkLog = debug('ai:sink')
-const mult2 = (value: number) => {
-  mapLog('mapping value')
-
-  return value * 2
+const doFunc = (value: number) => {
+  mapLog(`got value ${value}`)
 }
 
-const amult2 = async (value: number) => {
-  mapLog('mapping value begin')
+const asyncDoFunc = async (value: number) => {
+  mapLog(`getting value begin ${value}`)
   await wait(50)
-  mapLog('mapping value done')
-
-  return value * 2
+  mapLog(`getting value done ${value}`)
 }
 
-const emult2 = () => {
-  throw new Error('error in mapper')
+const errorDoFunc = () => {
+  throw new Error('error')
 }
 
 
-describe('[ pushMap ]', () => {
+describe.only('[ pushDo ]', () => {
   it('should work', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pushConsumer({ log: consumerLog })(spy)
-    const t = pushMap(mult2)
+    const t = pushDo(doSpy)
     const r = pushProducer({ log: producerLog })(data)
 
     await r(t(w))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
       [{ value: 2, done: false }],
-      [{ value: 4, done: false }],
-      [{ value: 6, done: false }],
+      [{ value: 3, done: false }],
       [{ value: undefined, done: true }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
+      [2],
+      [3],
     ])
   })
 
-  it('should work with async map', async () => {
+  it('should work with async dofunc', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(asyncDoFunc)
     const w = pushConsumer({ log: consumerLog })(spy)
-    const t = pushMap(amult2)
+    const t = pushDo(doSpy)
     const r = pushProducer({ log: producerLog })(data)
 
     await r(t(w))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
       [{ value: 2, done: false }],
-      [{ value: 4, done: false }],
-      [{ value: 6, done: false }],
+      [{ value: 3, done: false }],
       [{ value: undefined, done: true }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
+      [2],
+      [3],
     ])
   })
 
   it('should deliver consumer cancel', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pushConsumer({ log: consumerLog, cancelAtStep: 1 })(spy)
-    const t = pushMap(mult2)
+    const t = pushDo(doSpy)
     const r = pushProducer({ log: producerLog })(data)
 
     await r(t(w))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
-      [{ value: 2, done: false }],
+      [{ value: 1, done: false }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
     ])
   })
 
   it('should handle consumer crash', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pushConsumer({ log: consumerLog, crashAtStep: 1 })(spy)
-    const t = pushMap(mult2)
+    const t = pushDo(doSpy)
     const r = pushProducer({ log: producerLog })(data)
 
     await r(t(w))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
     ])
   })
 
   it('should deliver producer error to consumer', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
+    const doSpy = fn(doFunc)
     const w = pushConsumer({ log: consumerLog })(spy)
-    const t = pushMap(mult2)
+    const t = pushDo(doSpy)
     const r = pushProducer({ log: producerLog, errorAtStep: 2 })(data)
 
     await r(t(w))
 
     expect(spy.calls).deep.eq([
       [{ value: 0, done: false }],
-      [{ value: 2, done: false }],
+      [{ value: 1, done: false }],
+    ])
+
+    expect(doSpy.calls).deep.eq([
+      [0],
+      [1],
     ])
   })
 
-  it('should deliver mapper error to consumer', async () => {
+  it('should not deliver dofunc error to consumer', async () => {
     const data = makeNumbers(4)
     const spy = fn(sinkLog)
     const w = pushConsumer({ log: consumerLog })(spy)
-    const t = pushMap(emult2)
+    const t = pushDo(errorDoFunc)
     const r = pushProducer({ log: producerLog })(data)
 
     await r(t(w))
 
-    expect(spy.calls).deep.eq([])
+    expect(spy.calls).deep.eq([
+      [{ value: 0, done: false }],
+      [{ value: 1, done: false }],
+      [{ value: 2, done: false }],
+      [{ value: 3, done: false }],
+      [{ value: undefined, done: true }],
+    ])
   })
 })
